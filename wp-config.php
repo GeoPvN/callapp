c05af1745012fecfe221f48aa7910789770e6159
datopapala <dpapalashvili@yahoo.com>
2015-05-22 20:56:24 +0400

    sss

--- wp-config.php
+++ wp-config.php
@@ -16,10 +16,10 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'adrenali_wordpress');
+define('DB_NAME', 'callapp');
 
 /** MySQL database username */
-define('DB_USER', 'adrenali_root');
+define('DB_USER', 'root');
 
 /** MySQL database password */
 define('DB_PASSWORD', 'Gl-1114');
